61e1923a49685315fc4b77212eaae93ee1a948ed
Emokui <136175327+Emokui@users.noreply.github.com>
2024-09-26 16:08:03 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <div style="margin-top: 15px;">
         <div style="width: fit-content;float: left;">
           <div class="icon">
-            <img src="https://sukuna.ygking.lol/raw/Sukuna.png" alt="描述" style="width: 50px; height: 50px;">
+            <img src="https://sukuna.ygking.lol/raw/Sukuna.png" alt="描述" style="width: 40px; height: 40px;">
           </div>
           <span class="title"> Net-Panel </span>
         </div>
